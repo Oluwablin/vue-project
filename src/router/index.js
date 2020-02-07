3d966c76612eada0805b9a997de1a1d3a716b8b2
oluwablin <oyelekeseun@outlook.com>
2020-02-07 10:58:51 +0100

    route for editing book added

--- src/router/index.js
+++ src/router/index.js
@@ -33,6 +33,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Book.vue')
+  },
+  {
+    path: '/books/:id/edit',
+    name: 'editbook',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/EditBook.vue')
   }
 ]
 
