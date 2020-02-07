f3c26d606e02987e9189e6f3ab1611294af98a0f
oluwablin <oyelekeseun@outlook.com>
2020-02-07 10:59:46 +0100

    file for adding book created

--- src/views/AddBook.vue
+++ src/views/AddBook.vue
@@ -1,41 +1,48 @@
 <template>
     <div class="create">
         <h1>Create Book</h1>
-     <form action="#" method="POST">
+     <form action="#" method="POST" @submit.prevent="addBook">
           <div class="form-group">
               <label for="title">Title</label>
-              <input type="text" name="title" id="title">
+              <input type="text" name="title" id="title" v-model="title">
           </div> 
           <div class="form-group">
               <label for="title">Author</label>
-              <input type="text" name="author" id="author">
+              <input type="text" name="author" id="author" v-model="author">
           </div>
           <div class="form-group">
               <label for="title">Image</label>
-              <input type="text" name="image" id="image">
+              <input type="text" name="image" id="image" v-model="image">
           </div>
           <div class="form-group">
               <label for="title">Description</label>
-              <textarea name="description" id="description" cols="30" rows="10"></textarea>
+              <textarea name="description" id="description" cols="30" rows="10" v-model="description"></textarea>
           </div>
           <div class="form-group">
               <label for="title">Link</label>
-              <input type="text" name="link" id="link">
+              <input type="text" name="link" id="link" v-model="link">
           </div>
           <div class="form-group">
-              <label for="title">Title</label>
-              <input type="text" name="title" id="title">
-          </div>
-          <div class="form-group">
-              <label><input type="checkbox" name="featured">Featured</label>
+              <label><input type="checkbox" name="featured" v-model="featured">Featured</label>
           </div>
          <div class="form-group">
-             <select name="category" id="category" class="input-field input-normal">
+             <!-- <select name="category" id="category" class="input-field input-normal">
              <option value="cat1">Category 1</option>
              <option value="cat2">Category 2</option>
              <option value="cat3">Category 3</option>
-             </select>
+             </select> -->
+             <ApolloQuery :query="require('@/graphql/queries/Categories.gql')">
+               <template slot-scope="{ result: { data, loading } , isLoading }">
+                 <div v-if="isLoading">Loading...</div>
+                    <select v-else v-model="category">
+                       <option v-for="category of data.categories" :key="category.id" value="category.id">
+                          {{ category.name }}
+                       </option>
+                   </select>
+                </template>
+             </ApolloQuery>
           </div>
+
           <div class="form-group">
               <button type="submit">Add Book</button>
           </div>
@@ -43,7 +50,52 @@
     </div>
 </template>
 
-// let us style our form here
+<!-- let us make some scripts here -->
+<script>
+import addBook from '@/graphql/mutations/CreateBook.gql'
+
+export default {
+    
+    data() {
+        return {
+            title: '',
+            author: '',
+            image: '',
+            description: '',
+            link: '',
+            featured: false,
+            category_id: 1
+            }
+    },
+    methods: {
+        addBook() {
+            // Call to the graphql mutation
+    this.$apollo.mutate({
+      // Query
+      mutation: addBook,
+      // Parameters
+      variables: {
+        title: this.title,
+        author: this.author,
+        image: this.image,
+        link: this.link,
+        description: this.description,
+        featured: this.featured,
+        category_id: this.category_id
+      }
+    }).then((data) => {
+      console.log(data)
+      this.$router.push('/')
+    }).catch((error) => {
+      console.error(error)
+    })
+            // alert('adding new book')
+        }
+    }
+}
+</script>
+
+<!-- let us style our form here -->
 
 <style scoped>
    .form-group {
